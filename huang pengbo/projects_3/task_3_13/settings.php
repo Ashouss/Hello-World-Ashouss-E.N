20745a6629491c0511d3bfbad7fc39be56d9f877
Ashouss <jackthehere@gmail.com>
2026-04-16 13:01:12 +0300

    Update settings.php

--- huang pengbo/projects_3/task_3_13/settings.php
+++ huang pengbo/projects_3/task_3_13/settings.php
@@ -6,9 +6,7 @@ return [
     'db_user' => 'app_user',
     'db_pass' => 'secret',
 
-    // Путь к каталогу данных MySQL
     'db_data_path' => '/var/lib/mysql/data',
 
-    // Дополнительные настройки
     'log_path' => '/var/log/app.log'
 ];
